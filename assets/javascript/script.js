3ceca27a405b5dea3145b12bd2671f4e9deb3c01
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-09-21 17:18:04 -0500

    topic buttons hooked up to API request

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,17 @@
 var metalArray = ["thrash", "death", "black", "djent", "nu", "speed"]
-var apiURL = 'https://api.giphy.com/v1/gifs/search';
- $("#test").on("click", function(){
+for (var m = 0; m < metalArray.length; m++){
+    var genre = $("<button>");
+    genre.html(metalArray[m]);
+    genre.attr("name", metalArray[m]);
+    genre.attr("class", "searchButton");
+    $("#buttons").append(genre);
+}
+$(".searchButton").on("click", function(){
+    var genreName = $(this).attr("name")
     $.ajax({
         method: "GET",
         url: apiURL + '?' + $.param({
-            q: "test",    
+            q: genreName + " metal",  
             api_key: 'kzmA9HrLpUOdpCLEXes0liGMs7ZJmPpG',
             limit: 10
         }),
@@ -16,7 +23,28 @@ var apiURL = 'https://api.giphy.com/v1/gifs/search';
             var imageUrl = response.data[x].images.original.url;
             var testImage = $("<img>");
             testImage.attr("src", imageUrl);
-            testImage.attr("data-state", "still");
+            testImage.attr("alt", "test image");
+            $("#images").append(testImage)    
+        }
+    })
+})
+var apiURL = 'https://api.giphy.com/v1/gifs/search';
+ $("#test").on("click", function(){
+    $.ajax({
+        method: "GET",
+        url: apiURL + '?' + $.param({
+            q: "metal",    
+            api_key: 'kzmA9HrLpUOdpCLEXes0liGMs7ZJmPpG',
+            limit: 10
+        }),
+        
+    })
+    .then(function(response){
+        console.log(response);
+        for (var x = 0; x < response.data.length; x++){
+            var imageUrl = response.data[x].images.original.url;
+            var testImage = $("<img>");
+            testImage.attr("src", imageUrl);
             testImage.attr("alt", "test image");
             $("#images").append(testImage)    
         }
