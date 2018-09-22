b778aba5f5b301cea30eac5f5e659aaedb8c505b
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-09-22 05:28:20 -0500

    final functionality additions, styling, updated README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# giftastic
+a simple metal-themed page that utilizes a GIPHY API
+-pulls 10 static gifs from GIPHY.com and displays them on page with rating
+-allows user to click to animate or pause individual gifs
+-user can add buttons with input form
+-pulls gifs based on button keyword + "metal"
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,5 @@
 var metalArray = ["Thrash", "Death", "Black", "Viking", "Folk", "Speed"]
+var apiURL = 'https://api.giphy.com/v1/gifs/search';
 for (var m = 0; m < metalArray.length; m++) {
     var genre = $("<button>");
     genre.html(metalArray[m]);
@@ -38,44 +39,52 @@ $(".searchButton").on("click", function () {
 
         })
 })
-//test button
-var apiURL = 'https://api.giphy.com/v1/gifs/search';
-$("#test").on("click", function () {
-    $.ajax({
-        method: "GET",
-        url: apiURL + '?' + $.param({
-            q: "metal",
-            api_key: 'kzmA9HrLpUOdpCLEXes0liGMs7ZJmPpG',
-            limit: 10
-        }),
+$(document).on("click", ".gif", function () {
 
-    })
-        .then(function (response) {
-            console.log(response);
-            for (var x = 0; x < response.data.length; x++) {
-                var imageUrl = response.data[x].images.original_still.url;
+    var state = $(this).attr("data-state");
 
-                var testImage = $("<img>");
-                testImage.attr("src", imageUrl);
-                testImage.attr("alt", "test image");
-                var rate = $("<p>");
-                rate.text("rating: " + response.data[x].rating);
-                $("#images").append(testImage, rate);
-            }
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
+$("#add").on("click", function (event) {
+    event.preventDefault();
+    var inputValue = $("#another").val().trim();
+        $.ajax({
+            method: "GET",
+            url: apiURL + '?' + $.param({
+                q: inputValue + " metal",
+                api_key: 'kzmA9HrLpUOdpCLEXes0liGMs7ZJmPpG',
+                limit: 10
+            }),
 
         })
+            .then(function (response) {
+                //console.log(response);
+                $("#images").empty();
+                for (var x = 0; x < response.data.length; x++) {
+                    var imageUrl = response.data[x].images.original_still.url;
+                    var animatedUrl = response.data[x].images.original.url;
+                    var testImage = $("<img>");
+                    testImage.attr("src", imageUrl);
+                    //testImage.attr("alt", "test image");
+                    testImage.attr("data-state", "still");
+                    testImage.attr("data-still", imageUrl);
+                    testImage.attr("data-animate", animatedUrl);
+                    testImage.addClass("gif");
+                    var rate = $("<p>");
+                    rate.text("rating: " + response.data[x].rating);
+                    $("#images").append(testImage, rate);
+                }
 
-
-});
-$(document).on("click", ".gif", function() {
-    
-    var state = $(this).attr("data-state");
-   
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-    } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-    }
-  });
+            })
+        var newButton = $("<button>");
+        newButton.html(inputValue);
+        newButton.attr("name", inputValue);
+        newButton.attr("class", "searchButton");
+        $("#newButtons").append(newButton);
+})
